d1a2dd3a404c8f25c34bcdb713ecc91f1e4fd972
juliana-zlatevski <juliana.zlatevski@hotmail.com>
2020-09-23 19:01:22 -0500

    commit 9 - added functionality to welcome pop-up display, features for hiding & displaying as needed

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,17 @@
+// removing overlay pop-up message
+
 $('.popup-close').on('click', () => {
     $('.popup-overlay').hide();
+})
+
+$('.popup-btn').on('click', () => {
+    const $playerName = $('.popup-name').val();
+    $('.name').text($playerName);
+    $('.popup-overlay').hide();
+})
+
+// bringing back overlay pop-up message
+
+$('.help-btn').on('click', () => {
+    $('.popup-overlay').show();
 })
