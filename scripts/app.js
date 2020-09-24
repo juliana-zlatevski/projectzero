7bde07cc01ec3b299d624712cb13664e7ff2919e
juliana-zlatevski <juliana.zlatevski@hotmail.com>
2020-09-24 16:33:26 -0500

    created pet class & function to instantiate -- connected to pop-up window buttons

--- scripts/app.js
+++ scripts/app.js
@@ -8,6 +8,7 @@ $('.popup-btn').on('click', () => {
     const $playerName = $('.popup-name').val();
     $('.name').text($playerName);
     $('.popup-overlay').hide();
+    createAlien();
 })
 
 // bringing back overlay pop-up message
@@ -15,3 +16,23 @@ $('.popup-btn').on('click', () => {
 $('.help-btn').on('click', () => {
     $('.popup-overlay').show();
 })
+
+// creating pet class
+
+class AlienPet {
+    constructor() {
+        this.hunger = 1;
+        this.sleepiness = 1;
+        this.boredom = 1;
+        this.age = 0;
+    }
+}
+
+// instantiating object on click
+
+function createAlien () {
+    const player = new AlienPet();
+    console.log(`created successfully`);
+    console.log(player);
+}
+
